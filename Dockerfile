dd73b1baaf900d8fd672706262fd1b72d752d5df
Will Ruggiano <wmruggiano@gmail.com>
2021-06-15 03:29:04 -0400

    Minor tweaks to forker repo

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,27 @@
 FROM golang:1.15 as build
 
-ARG TAG="v1.9.24"
+ARG TAG=v1.10.3
 
-RUN apt-get update && apt-get install -y \
-    make \
-    gcc \
-    libc6-dev \
-    git; \
-    apt-get clean; \
-    rm -rf /var/lib/apt/lists/*;
+RUN apt-get update && apt-get install -y make gcc libc6-dev git && apt-get clean && rm -rf /var/lib/apt/lists/*
 
-RUN git clone https://github.com/ethereum/go-ethereum.git && cd /go/go-ethereum && \
-    if [[ -n $TAG ]]; then git checkout $TAG; fi && env GO111MODULE=on go run build/ci.go install ./cmd/clef
+RUN git clone https://github.com/ethereum/go-ethereum.git \
+    && cd /go/go-ethereum \
+    && git checkout $TAG \
+    && env GO111MODULE=on go run build/ci.go install ./cmd/clef
+
+RUN mkdir -p /out/usr/local/bin
+RUN cp /go/go-ethereum/build/bin/clef /out/usr/local/bin/clef
+COPY packaging/rules.js /out/app/config/rules.js
+COPY packaging/4byte.json /out/app/config/4byte.json
+COPY packaging/docker/entrypoint.sh /out/entrypoint.sh
+
+#RUN chown -R nobody:nogroup /out
 
 FROM debian:10.2-slim as runtime
-
-RUN mkdir -p /app/data && chown nobody:nogroup /app/data
-
-COPY --from=build /go/go-ethereum/build/bin/clef /usr/local/bin/clef
-COPY packaging/rules.js /app/config/rules.js
-COPY packaging/4byte.json /app/config/4byte.json
-COPY packaging/docker/entrypoint.sh /entrypoint.sh
+COPY --from=build /out /
 
 EXPOSE 8550
-USER nobody
+#USER nobody
 WORKDIR /app
 VOLUME /app/data
 
--- packaging/docker/entrypoint.sh
+++ packaging/docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ACTION="${1:-run}"
-CHAINID="${CLEF_CHAINID:-12345}"
+ACTION="${1:-full}"
+CHAINID="${CLEF_CHAINID:-5}"
 DATA=/app/data
 
 init() {
@@ -10,28 +10,28 @@ init() {
         < /dev/urandom tr -dc _A-Z-a-z-0-9 2> /dev/null | head -c32 > "$DATA"/password
     fi
     SECRET=$(cat "$DATA"/password)
-    /usr/local/bin/clef --configdir "$DATA" --stdio-ui init >/dev/null 2>&1 << EOF
+    clef --configdir "$DATA" --stdio-ui --suppress-bootwarn init >/dev/null 2>&1 << EOF
 $SECRET
 $SECRET
 EOF
     if [ "$(ls -A "$DATA"/keystore 2> /dev/null)" = "" ]; then
-        /usr/local/bin/clef --keystore "$DATA"/keystore --stdio-ui newaccount --lightkdf >/dev/null 2>&1 << EOF
+        clef --keystore "$DATA"/keystore --stdio-ui newaccount --lightkdf >/dev/null 2>&1 << EOF
 $SECRET
 EOF
     fi
-    /usr/local/bin/clef --keystore "$DATA"/keystore --configdir "$DATA" --stdio-ui setpw 0x"$(parse_json "$(cat "$DATA"/keystore/*)" address)" >/dev/null 2>&1 << EOF
+    clef --keystore "$DATA"/keystore --configdir "$DATA" --stdio-ui setpw 0x"$(parse_json "$(cat "$DATA"/keystore/*)" address)" >/dev/null 2>&1 << EOF
 $SECRET
 $SECRET
 $SECRET
 EOF
-    /usr/local/bin/clef --keystore "$DATA"/keystore --configdir "$DATA" --stdio-ui attest "$(sha256sum /app/config/rules.js | cut -d' ' -f1 | tr -d '\n')" >/dev/null 2>&1 << EOF
+    clef --keystore "$DATA"/keystore --configdir "$DATA" --stdio-ui attest "$(sha256sum /app/config/rules.js | cut -d' ' -f1 | tr -d '\n')" >/dev/null 2>&1 << EOF
 $SECRET
 EOF
 }
 
 run() {
     SECRET=$(cat "$DATA"/password)
-    rm /tmp/stdin /tmp/stdout || true
+    rm -f /tmp/stdin /tmp/stdout || true
     mkfifo /tmp/stdin /tmp/stdout
     (
     exec 3>/tmp/stdin
@@ -43,7 +43,7 @@ run() {
         fi
     done
     ) &
-    /usr/local/bin/clef --stdio-ui --keystore "$DATA"/keystore --configdir "$DATA" --chainid "$CHAINID" --http --http.addr 0.0.0.0 --http.port 8550 --http.vhosts "*" --rules /app/config/rules.js --nousb --lightkdf --ipcdisable --4bytedb-custom /app/config/4byte.json --pcscdpath "" --auditlog "" --loglevel 3 < /tmp/stdin | tee /tmp/stdout
+    clef --stdio-ui --suppress-bootwarn --keystore "$DATA"/keystore --configdir "$DATA" --chainid "$CHAINID" --http --http.addr 0.0.0.0 --http.port 8550 --http.vhosts "*" --rules /app/config/rules.js --nousb --lightkdf --ipcdisable --4bytedb-custom /app/config/4byte.json --pcscdpath "" --auditlog "" --loglevel 3 < /tmp/stdin | tee /tmp/stdout
 }
 
 full() {
